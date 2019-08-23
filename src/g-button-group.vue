391307b09d1dca7727992ba3ec0d772c0d55c6df
wuxuwei <2509165479@qq.com>
2019-08-23 21:52:45 +0800

    检测子元素是不是button

--- src/g-button-group.vue
+++ src/g-button-group.vue
@@ -6,7 +6,14 @@
 
 <script>
     export default {
-        name: "g-button-group"
+        name: "g-button-group",
+        mounted() {
+            for(let node of this.$el.children) {
+                if(node.nodeName.toLowerCase() !== 'button') {
+                    console.warn('g-button-group 的子元素应该全是 g-button')
+                }
+            }
+        }
     }
 </script>
 
