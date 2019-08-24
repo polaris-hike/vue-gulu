5d84c0954586d829a007aaa60240d01888be5ad8
wuxuwei <2509165479@qq.com>
2019-08-24 11:25:13 +0800

    安装chai

--- src/g-button-group.vue
+++ src/g-button-group.vue
@@ -23,7 +23,9 @@
         vertical-align: center;
         > .g-button {
             border-radius: 0;
-            margin-left: -1px;
+            &:not(:first-child) {
+                margin-left: -1px;
+            }
             &:first-child {
                 border-top-left-radius: var(--border-radius);
                 border-bottom-left-radius: var(--border-radius);
